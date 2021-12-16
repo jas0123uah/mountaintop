5f7c02e4dd00f94a7c5fb2ae8d887f1e202f53fa
jas0123uah <jas0123@uah.edu>
2021-12-16 07:59:57 -0500

    Added getaways GET route for getaways.

---
+++ app/api/getaway_routes.py
@@ -0,0 +1,16 @@
+import re
+from flask import Blueprint, jsonify, request
+from flask_login import login_required
+
+
+from app.models import db, Getaway
+#from app.forms import NewRestaurant, RestaurantOwnerReservationForm, CustomerReservationForm, EditRestaurant
+
+from .auth_routes import validation_errors_to_error_messages
+
+getaway_routes = Blueprint('getaways', __name__)
+
+@getaway_routes.route('/')
+def get_most_recent_getaways():
+    recent_getaways = db.session.query(Getaway).order_by(Getaway.id.desc()).limit(5)
+    return {'getaways': [getaway.to_dict() for getaway in recent_getaways]}
