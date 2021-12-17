76443a93567e2591c461832773de6f892d397ec0
jas0123uah <jas0123@uah.edu>
2021-12-16 19:15:57 -0500

    Removed incorrect import

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,4 @@
 from .db import db
-from .getaway import get_reservations
 import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
