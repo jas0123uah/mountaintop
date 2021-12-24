a51ab7a13fa4b679884cb709e54eaa9c939fe03d
jas0123uah <jas0123@uah.edu>
2021-12-24 18:28:24 -0500

    Sign up modal

---
+++ react-app/src/components/SignUpModal/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { Modal } from '../../context/Modal';
+import SignUpForm from './SignUpForm';
+
+function SignUpFormModal() {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+    
+      <i class="fas fa-user-plus fa-3x" onClick={() => {setShowModal(true);}}></i>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <SignUpForm />
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default SignUpFormModal;
