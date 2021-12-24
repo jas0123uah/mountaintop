b43c7fcb81a4bb5a5251395a95193d79f1e54d59
jas0123uah <jas0123@uah.edu>
2021-12-24 18:06:25 -0500

    New user needs first name, last name, & pfp.

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -63,9 +63,11 @@ def sign_up():
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         user = User(
-            username=form.data['username'],
+            firstName = form.data['firstName'],
+            lastName = form.data['lastName'],
+            profilePictureUrl=form.data['profilePicture'],
             email=form.data['email'],
-            password=form.data['password']
+            hashed_password=form.data['password']
         )
         db.session.add(user)
         db.session.commit()
