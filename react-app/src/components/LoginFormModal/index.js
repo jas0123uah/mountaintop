71b6fc89dd48627d2e8718a680f6d39fb7009c53
jas0123uah <jas0123@uah.edu>
2021-12-24 18:33:40 -0500

    Login form modal

---
+++ react-app/src/components/LoginFormModal/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { Modal } from '../../context/Modal';
+import LoginForm from './LoginForm';
+
+function LoginFormModal() {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+    
+      <i class="fa fa-sign-in fa-3x" onClick={() => {setShowModal(true);}}></i>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <LoginForm />
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default LoginFormModal;
