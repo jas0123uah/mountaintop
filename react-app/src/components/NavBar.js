112cc3d6b4c2a0ba2ce2a75e18b82ca05ffc4a33
jas0123uah <jas0123@uah.edu>
2021-12-25 22:00:00 -0500

    Removed component import that was used in testing

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
-import LoginSignUpDropDownMenu from '../components/LoginSignUpDropDownMenu' 
 import LoginFormModal from './LoginFormModal';
 import SignUpFormModal from './SignUpModal'
 const NavBar = () => {
