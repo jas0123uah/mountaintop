b93ced8e6c8194e311688b1c1c682049dc8ba8f8
jas0123uah <jas0123@uah.edu>
2021-12-28 18:07:45 -0500

    WIP BookReservation

--- react-app/src/components/BookReservationForm/index.js
+++ react-app/src/components/BookReservationForm/index.js
@@ -1,45 +1,58 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect , useHistory, useParams } from 'react-router-dom';
-//import DatePicker from "react-datepicker";
-//import "react-datepicker/dist/react-datepicker.css";
+import moment from "moment"
 import {createReservation} from '../../store/session'
 import $ from "jquery";
+import "jquery-ui";
+import 'jquery-ui/ui/widgets/datepicker'
+import 'jquery-ui/themes/base/core.css'
+import 'jquery-ui/themes/base/datepicker.css'
+import 'jquery-ui/themes/base/theme.css'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+//import { Row, Col, Form, FormGroup, Label, Input, Button } from "reactstrap";
+// import 'jquery-ui/themes/base/core.css';
+// import 'jquery-ui/themes/base/theme.css';
+// import 'jquery-ui/themes/base/selectable.css';
+// import 'jquery-ui/ui/core';
+// import 'jquery-ui/ui/widgets/selectable';
 //import $ from '../node_modules';
 export const NewReservation = () => {
   const [errors, setErrors] = useState([]);
-  const [startDate, setStartDate] = useState('');
+  // const [startDate, setStartDate] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState('');
   const user = useSelector(state => state.session.user);
   const userId = user?.id
   const { getawayId }  = useParams();
-//   var $j = $.noConflict();
+  console.log(startDate)
 
   const dispatch = useDispatch();
   const history = useHistory();
-//   var array = ["2021-12-29","2013-03-15","2013-03-16"]
-//   $( function() {
-//   $j('#datepicker').datepicker({
-//     beforeShowDay: function(date){
-//         var string = $('#datepicker').datepicker.formatDate('yy-mm-dd', date);
-//         return [ array.indexOf(string) == -1 ]
-//     }
-// })
-// });
+  var array = ["2021-12-29","2013-03-15","2013-03-16"]
+  $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+
+
+
+const isWeekday = (date) => {
+    const day = new Date(date);
+    console.log(day, "AGG")
+    day.getDay()
+    return day.getDay() !== 0 && day.getDay() !== 6;
+  };
+
 
   
 
   const handleSubmitReservation = async (e) => {
-    dispatch(createReservation({getawayId, startDate, endDate, userId})).catch(
-      async (res) => {
-        const data = await res.json();
-        if (data && data.errors) setErrors(data.errors);
-      }
-    );
-   // history.push('/profile/')
-    //window.location.reload()
+    dispatch(createReservation({getawayId, startDate, endDate, userId}))
+    history.push('/profile')
 
   }
+
 
   return (
     <div className='formWrapper'onSubmit={handleSubmitReservation}>
@@ -51,29 +64,12 @@ export const NewReservation = () => {
           <div key={ind}>{error}</div>
         ))}
       </div>
-      <div className="input-div">
-          <label htmlFor="start_date">Start Date:</label>
-          <input
-            type="date"
-            name="start-date"
-            min="2021-12-09"
-            max="2022-12-31"
-            value={startDate}
-            onChange={(e) => setStartDate(e.target.value)}
-          ></input>
-        </div>
-       <div className="input-div">
-          <label htmlFor="end_date">End Date:</label>
-          <input
-            type="date"
-            name="end-date"
-            min={startDate}
-            max="2022-12-31"
-            value={endDate}
-            onChange={(e) => setEndDate(e.target.value)}
-          ></input>
-        </div>
-        <p>Date:<input id="datepicker" type="text"></input></p>
+    
+        <DatePicker selected={startDate} format="yyyy-MM-dd" filterDate={isWeekday}  onChange={(date) => setStartDate(date)} minDate={new Date()} maxDate={new Date(endDate)} />
+
+        <DatePicker selected={endDate} format="yyyy-MM-dd"
+        minDate={new Date(startDate)} filterDate={isWeekday}  onChange={(date) => setEndDate(date)} />
+        <button type="submit" className="createGetawaySubmitButton">Book Reservation</button>
       </fieldset>
 
 
