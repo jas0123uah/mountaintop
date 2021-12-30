d81a763a4ff4155f50bb0e36da43d4b0e2c488b2
jas0123uah <jas0123@uah.edu>
2021-12-30 02:28:15 -0500

    Homepage with random getaway.

---
+++ react-app/src/components/HomePage/index.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+import GetawayCarousel from "../Carousel";
+
+
+export const HomePage = () => {
+    
+    //console.log(Object.values(randomGetawayImagesObjs));
+
+    return (<div>
+        <GetawayCarousel/>
+        {/* <div>
+            <p>Welcome to Mountaintop! Mountaintop is your destination for booking the most beautiful getaways in the Great Smoky Mountains!</p>
+            <button>Check this getaway</button>
+        </div> */}
+        </div>)
+        
+}
