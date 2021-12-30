7404ec9185401bdcbd94b5ce1155406cf59c60ad
jas0123uah <jas0123@uah.edu>
2021-12-30 02:22:39 -0500

    Added FA icon

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
-
+import {useHistory } from 'react-router-dom';
 const LogoutButton = () => {
+  const history = useHistory();
   const dispatch = useDispatch()
   const onLogout = async (e) => {
     await dispatch(logout());
+    history.push('/')
   };
 
-  return <button onClick={onLogout}>Logout</button>;
+  return  <i onClick={onLogout} class="fas fa-sign-out-alt fa-3x"></i> ;
 };
 
 export default LogoutButton;
