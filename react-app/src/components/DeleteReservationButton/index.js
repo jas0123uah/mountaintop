2c1272620b4b652218c6dbaa9836e22f3ee5b27e
jas0123uah <jas0123@uah.edu>
2022-01-02 20:47:27 -0500

    Delete reservation button

--- react-app/src/components/DeleteReservationButton/index.js
+++ react-app/src/components/DeleteReservationButton/index.js
@@ -2,6 +2,8 @@ import {deleteReservation} from '../../store/session'
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect , useHistory, useParams } from 'react-router-dom';
+import {loadGetaways} from '../../store/getaways'
+import {authenticate} from '../../store/session'
 export const DeleteReservationButton = (item) =>{
   const user = useSelector(state => state.session.user);
   const [errors, setErrors] = useState([]);
@@ -9,16 +11,16 @@ export const DeleteReservationButton = (item) =>{
   const history = useHistory();
   const dispatch = useDispatch();
   const handleDeleteReservation = async (id) => {
-    dispatch(deleteReservation(id)).catch(
+    await dispatch(deleteReservation(id)).catch(
       async (res) => {
         const data = await res.json();
         if (data && data.errors) setErrors(data.errors);
       }
     );
-    history.push('/profile/')
-    window.location.reload()
+    await dispatch(loadGetaways()).then((res) => dispatch (authenticate()))
+      }
 
-  }
+  
   return (
       <form onSubmit={(item) =>handleDeleteReservation(item.currentTarget.dataset.id)} data-id={item.id}>
           Cancel reservation
