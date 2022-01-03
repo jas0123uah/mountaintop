7ebc970d7fae2e18da2ede6ee41f4e87dda84bf1
jas0123uah <jas0123@uah.edu>
2022-01-02 20:05:37 -0500

    Added image form

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -3,3 +3,4 @@ from .signup_form import SignUpForm
 from .getaway_form import GetawayForm
 from .review_form import ReviewForm
 from .reservation_form import ReservationForm
+from .image_form import ImageForm
