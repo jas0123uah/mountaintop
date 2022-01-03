9621b787629682d76f9c19051ff44e3826428942
jas0123uah <jas0123@uah.edu>
2022-01-02 20:46:56 -0500

    Book reservation modal

--- react-app/src/components/BookReservationModal/index.js
+++ react-app/src/components/BookReservationModal/index.js
@@ -1,19 +1,26 @@
 import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
+import { useDispatch, useSelector } from "react-redux";
+import LoginFormModal from '../LoginFormModal';
 
 import {BookReservationModalForm} from './BookReservationModalForm'
+import { useEffect } from 'react';
 function BookReservationModal() {
   const [showModal, setShowModal] = useState(false);
+  const user = useSelector(state => state.session.user);
+  const bookingButton= <BookReservationModalForm/>
+  const buttonToDisplay = user ? bookingButton: <LoginFormModal/>
 
   return (
     <>
-    
-      <button  onClick={() => {setShowModal(true);}}>Book a reservation</button>
-      {showModal && (
+    {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <BookReservationModalForm />
         </Modal>
       )}
+    {buttonToDisplay}
+    
+      
     </>
   );
 }
