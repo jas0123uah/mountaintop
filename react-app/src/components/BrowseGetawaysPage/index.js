4e0a5a8106bddbc9066162e70da7cec6f9e6e13b
jas0123uah <jas0123@uah.edu>
2022-01-06 07:47:11 -0500

    Condensed map to return SingleGetawaySearchOrBrowseResult Component

--- react-app/src/components/BrowseGetawaysPage/index.js
+++ react-app/src/components/BrowseGetawaysPage/index.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import 'react-gallery-carousel/dist/index.css';
 import { useSelector } from "react-redux";
-import { NavLink} from 'react-router-dom';
+import {SingleGetawaySearchOrBrowseResult} from '../SingleGetawaySearchOrBrowseResult'
 const BrowseGetawaysPage = () => {
     const getawaysObject = useSelector(state => state.getaways);
     const getawaysArray = Object.values(getawaysObject)
   return (
     <div>
       {getawaysArray.map( getaway => {
-        return <div className="singleResult">
-          <img className="singleResultImage" src={`${Object.values(getaway.images)[0].url}`} alt="" />
-          <div className="singleResultInfo">
-          <NavLink to={`/getaways/${getaway.id}`} exact={true} activeClassName='active'>
-          <h1>{getaway.name}</h1>
-          </NavLink>
-          <h3>{`${getaway.numGuests} guests | ${getaway.numBedrooms} bedrooms | ${getaway.numBeds} beds | ${getaway.numBaths} baths`}</h3>
-          <h4> {`$${getaway.price}/night`}</h4>
-          </div>
-        </div>
+        return <SingleGetawaySearchOrBrowseResult getaway={getaway} key={getaway.id}/>
       })}
     </div>
     
