15fecebbf783aa6cdf130021bf0bc1bee01bbd99
jas0123uah <jas0123@uah.edu>
2022-01-06 07:50:00 -0500

    Remove unused imports

--- react-app/src/components/EditReservationModal/index.js
+++ react-app/src/components/EditReservationModal/index.js
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
-import { useDispatch, useSelector } from "react-redux";
+import {useSelector } from "react-redux";
 import LoginFormModal from '../LoginFormModal';
-
 import {EditReservationModalForm} from './EditReservationModalForm'
-import { useEffect } from 'react';
 function EditReservationModal() {
   const [showModal, setShowModal] = useState(false);
   const user = useSelector(state => state.session.user);
