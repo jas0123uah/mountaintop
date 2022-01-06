fe0f9fbf9e3f8cee26bc5ea896ccb0afa0eebc35
jas0123uah <jas0123@uah.edu>
2022-01-06 07:49:01 -0500

    Remove unused imports

--- react-app/src/components/DeleteReservationButton/index.js
+++ react-app/src/components/DeleteReservationButton/index.js
@@ -1,14 +1,10 @@
 import {deleteReservation} from '../../store/session'
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Redirect , useHistory, useParams } from 'react-router-dom';
+import {useDispatch } from 'react-redux';
 import {loadGetaways} from '../../store/getaways'
 import {authenticate} from '../../store/session'
 export const DeleteReservationButton = (item) =>{
-  const user = useSelector(state => state.session.user);
   const [errors, setErrors] = useState([]);
-  const userId = user?.id
-  const history = useHistory();
   const dispatch = useDispatch();
   const handleDeleteReservation = async (id) => {
     await dispatch(deleteReservation(id)).catch(
@@ -20,7 +16,6 @@ export const DeleteReservationButton = (item) =>{
     await dispatch(loadGetaways()).then((res) => dispatch (authenticate()))
       }
 
-  
   return (
       <form onSubmit={(item) =>handleDeleteReservation(item.currentTarget.dataset.id)} data-id={item.id}>
           Cancel reservation
