6057cd9cafcc37aaa749bde6fcee5a55e0dd3e07
jas0123uah <jas0123@uah.edu>
2022-01-10 17:06:43 -0500

    Getaway reservations also returns associated review if present

--- app/models/getaway.py
+++ app/models/getaway.py
@@ -1,6 +1,11 @@
 from .db import db
 import datetime
 from sqlalchemy.sql import func
+
+from sqlalchemy.orm.collections import (MappedCollection,
+                                        _SerializableAttrGetter,
+                                        collection,
+                                        _instrument_class)
 class Getaway(db.Model):
     __tablename__ = 'getaways'
     id = db.Column(db.Integer, primary_key=True)
@@ -47,9 +52,34 @@ class Getaway(db.Model):
             res_dict[reservation.id] = {"id":reservation.id,
                                         "getawayId":reservation.getawayId,
                                         "userId":reservation.userId,
+                                        'userProfilePicture': reservation.reservationUserId.profilePictureUrl,
+                                        'userFirstName': reservation.reservationUserId.firstName,
                                         "startDate":reservation.startDate,
                                         "endDate":reservation.endDate,
+                                        "reviewId":None,
+                                        "reviewText":None,
+                                        "cleanlinessRating":None,
+                                        "communicationRating":None,
+                                        "checkinRating":None,
+                                        "accuracyRating":None,
+                                        "locationRating":None,
+                                        "valueRating":None,
+                                        "overallRating":None,
                                         }
+            if reservation.reviews is not None:
+                dict_to_edit = res_dict[reservation.id]
+                dict_to_edit['reviewId'] = reservation.reviews.id
+                dict_to_edit['reviewText'] = reservation.reviews.reviewText
+                dict_to_edit['cleanlinessRating'] = reservation.reviews.cleanlinessRating
+                dict_to_edit['communicationRating'] = reservation.reviews.communicationRating
+                dict_to_edit['checkinRating'] = reservation.reviews.checkinRating
+                dict_to_edit['accuracyRating'] = reservation.reviews.accuracyRating
+                dict_to_edit['locationRating'] = reservation.reviews.locationRating
+                dict_to_edit['valueRating'] = reservation.reviews.valueRating
+                dict_to_edit['overallRating'] = reservation.reviews.overallRating
+                res_dict[reservation.id] = dict_to_edit
+            
+            
   
         return res_dict
     
@@ -66,6 +96,7 @@ class Getaway(db.Model):
                                         "locationRating":review.locationRating,
                                         "valueRating":review.valueRating,
                                         "overallRating":review.overallRating,
+                                        'userProfilePicture': review.reviewUserId.profilePictureUrl,
                                         }
         return rev_dict
     
