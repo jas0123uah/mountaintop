3af3432119631e11189d350465f8dabb44d39b8a
jas0123uah <jas0123@uah.edu>
2022-01-10 17:02:09 -0500

    Review routes return updated getaway dict

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -3,7 +3,7 @@ from flask import Blueprint, jsonify, request
 from flask_login import login_required
 from app.forms import ReviewForm
 from statistics import mean
-from app.models import db, Review
+from app.models import db, Review, Getaway
 
 from .auth_routes import validation_errors_to_error_messages
 
@@ -16,10 +16,14 @@ def post_review_for_getaway(id):
     if form.validate_on_submit():
         newReview = Review(getawayId = form.data['getawayId'], reviewText = form.data['reviewText'], cleanlinessRating = form.data['cleanlinessRating'], communicationRating = form.data['communicationRating'], checkinRating = form.data['checkinRating'], accuracyRating = form.data['accuracyRating'],locationRating = form.data['locationRating'],valueRating = form.data['valueRating'], 
                            overallRating = mean([form.data['cleanlinessRating'], 
-                                                 form.data['communicationRating'], form.data['checkinRating'], form.data['accuracyRating'], form.data['valueRating'], form.data['locationRating']]), userId=form.data['userId'])
+                                                 form.data['communicationRating'], form.data['checkinRating'], form.data['accuracyRating'], form.data['valueRating'], form.data['locationRating']]), userId=form.data['userId'], reservationsId = form.data['reservationsId'])
         db.session.add(newReview)
         db.session.commit()
-        return newReview.to_dict()
+        all_getaways = db.session.query(Getaway)
+        getawaydict ={}
+        for getaway in all_getaways:
+            getawaydict[getaway.id]=getaway.to_dict()
+        return getawaydict
     else:
         return {'errors': validation_errors_to_error_messages(form.errors)}, 400
 
@@ -37,9 +41,13 @@ def edit_review_by_id(id):
         reviewToEdit.accuracyRating = form.data['accuracyRating']
         reviewToEdit.locationRating = form.data['locationRating']
         reviewToEdit.valueRating = form.data['valueRating']
-        reviewToEdit.overallRating = mean([form.data['cleanlinessRating'], form.data['communicationRating'], form.data['checkinRating'], form.data['accuracyRating'], form.data['valueRating'], form.data['locationRating']])
+        reviewToEdit.overallRating = mean([form.data['cleanlinessRating'], form.data['communicationRating'], form.data['checkinRating'], form.data['accuracyRating'], form.data['valueRating'], form.data['locationRating']], reservationsId = form.data['reservationsId'])
         db.session.commit()
-        return reviewToEdit.to_dict()
+        all_getaways = db.session.query(Getaway)
+        getawaydict ={}
+        for getaway in all_getaways:
+            getawaydict[getaway.id]=getaway.to_dict()
+        return getawaydict
     else:
 
         return {'errors': validation_errors_to_error_messages(form.errors)}, 400
@@ -49,4 +57,8 @@ def delete_review_by_id(id):
     reviewToDelete = Review.query.get(int(id))
     db.session.delete(reviewToDelete)
     db.session.commit()
-    return {'message': f"Deleted review {id}"}
+    all_getaways = db.session.query(Getaway)
+    getawaydict ={}
+    for getaway in all_getaways:
+        getawaydict[getaway.id]=getaway.to_dict()
+    return getawaydict
