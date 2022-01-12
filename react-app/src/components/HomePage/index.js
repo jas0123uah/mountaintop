0b1945062e9dea6938864ec8814ca48a46310f48
jas0123uah <jas0123@uah.edu>
2022-01-12 13:15:20 -0500

    Home page features background images.

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import GetawayCarousel from "../Carousel";
+import BrowseGetawaysButton from '../BrowseGetawaysButton';
+import MountainsImage from '../assets/pexels-kalen-kemp-9560057.jpg'
 export const HomePage = () => {
     return (<div>
-        <GetawayCarousel/>
+        <img src={MountainsImage}  className={"splashBackground"}alt="" />
+        <div className="splashBlurb">
+            <span className="splashText">The mountains are calling.</span>
+            <br/>
+            <BrowseGetawaysButton/>
+            {/* <button className="browse-button">Browse getaways</button> */}
+        </div>
+        {/* <GetawayCarousel/> */}
         </div>)
         
 }
