6189e233e7ee51fddfcbf8b0252784d034abb70c
jas0123uah <jas0123@uah.edu>
2022-01-13 14:36:48 -0500

    Use search results filter so that browse results are filterable

--- react-app/src/components/BrowseGetawaysPage/index.js
+++ react-app/src/components/BrowseGetawaysPage/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import 'react-gallery-carousel/dist/index.css';
 import { useSelector } from "react-redux";
-import {SingleGetawaySearchOrBrowseResult} from '../SingleGetawaySearchOrBrowseResult'
+import { SearchResultsFilter } from '../SearchResultsFilter';
 const BrowseGetawaysPage = () => {
     const getawaysObject = useSelector(state => state.getaways);
     const getawaysArray = Object.values(getawaysObject)
   return (
-    <div className="searchResultsContainer">
-      {getawaysArray.map( getaway => {
-        return <SingleGetawaySearchOrBrowseResult getaway={getaway} key={getaway.id}/>
-      })}
+    <div className="browse">
+      <SearchResultsFilter searchResults={getawaysArray}/>
     </div>
     
   );
