6036c950a3b86f4ddb5934452a6b25e0680b50f8
jas0123uah <jas0123@uah.edu>
2022-01-13 13:36:27 -0500

    Share search filter across components

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -8,13 +8,16 @@ import './components/ProfilePage/carousel.css'
 import App from './App';
 import configureStore from './store';
 import { ModalProvider } from './context/Modal';
+import {SearchFilterProvider} from './context/SearchFilter';
 const store = configureStore();
 window.addEventListener('DOMContentLoaded', (event) => {
     ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ModalProvider>
+       <SearchFilterProvider>
         <App />
+        </SearchFilterProvider>
       </ModalProvider>
       </Provider>
   </React.StrictMode>,
