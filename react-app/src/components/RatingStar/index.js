07849a8388f8616be2656353150e94a71faf468c
jas0123uah <jas0123@uah.edu>
2022-01-15 19:06:35 -0500

    Deleted console.log

--- react-app/src/components/RatingStar/index.js
+++ react-app/src/components/RatingStar/index.js
@@ -6,9 +6,7 @@ export const RatingStar = ({ratingSetter, desiredRating, currentRating}) => {
     const filledStar = <i onClick={() => ratingSetter(desiredRating)} className={"fa fa-fas fa-star"} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}></i>
 
 
-    const borderStar = <i onClick={() => ratingSetter(desiredRating)} className={hovered ? "fa fa-fas fa-star":"far fa-star"} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}></i>
-    console.log(currentRating, "currentRating", desiredRating, "desiredRating") 
+    const borderStar = <i onClick={() => ratingSetter(desiredRating)} className={hovered ? "fa fa-fas fa-star":"far fa-star"} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}></i> 
     let starToReturn = currentRating>=desiredRating ? filledStar : borderStar
-    console.log(starToReturn, "starToReturn")
     return(starToReturn)
 }
