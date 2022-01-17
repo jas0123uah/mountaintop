0b7818760e08cda6af6525e85a6535f22e70a3a5
jas0123uah <jas0123@uah.edu>
2022-01-17 11:16:12 -0500

    Changed review button text

--- react-app/src/components/NewReviewButton/index.js
+++ react-app/src/components/NewReviewButton/index.js
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom"
 export const ReviewButton = ({getaways, reservation}) => {
     const NewReview = <NavLink to={`/getaways/${getaways[reservation.getawayId].id}/reservations/${reservation.id}/reviews/new`}><button className="viewGetawayButton">Leave a review</button></NavLink>
-    const EditReview = <NavLink to={`/getaways/${getaways[reservation.getawayId].id}/reservations/${reservation.id}/reviews/${reservation.reviewId}/edit`}><button className="viewGetawayButton">Edit your review</button></NavLink>
+    const EditReview = <NavLink to={`/getaways/${getaways[reservation.getawayId].id}/reservations/${reservation.id}/reviews/${reservation.reviewId}/edit`}><button className="viewGetawayButton">Edit Review</button></NavLink>
     return(
 <>
 {reservation.reviewId != null ? EditReview : NewReview} 
