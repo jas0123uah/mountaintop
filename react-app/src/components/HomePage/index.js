fb81d91fd5876b5069fab12c8cfd6ff0cf5fb24b
jas0123uah <jas0123@uah.edu>
2022-01-17 21:25:30 -0500

    Added class splash container

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -3,7 +3,7 @@ import GetawayCarousel from "../Carousel";
 import BrowseGetawaysButton from '../BrowseGetawaysButton';
 import MountainsImage from '../assets/pexels-kalen-kemp-9560057.jpg'
 export const HomePage = () => {
-    return (<div>
+    return (<div className="splashContainer">
         <img src={MountainsImage}  className={"splashBackground"}alt="" />
         <div className="splashBlurb">
             <span className="splashText">The mountains are calling.</span>
