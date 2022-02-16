c70f5fe4871832202f14b50a7afe40bb9df05925
jas0123uah <jas0123@uah.edu>
2022-02-16 17:19:55 -0500

    Detect mobile view

--- react-app/src/components/SearchComponents/SearchResultsFilter/index.js
+++ react-app/src/components/SearchComponents/SearchResultsFilter/index.js
@@ -1,19 +1,27 @@
-import React, {useContext } from 'react';
+import React, {useContext, useState } from 'react';
 import {SearchContext} from '../../../context/SearchFilter'
 import {getDesiredAmenities, compareDesiredAmenitiesToCurrentGetawaysAmenities} from '../../../utils/helperFunctions'
 import {SingleGetawaySearchOrBrowseResult} from '../SingleGetawaySearchOrBrowseResult'
+import {MobileSingleGetawaySearchOrBrowseResult} from '../../MobileSingleGetaway'
 
 
 export const SearchResultsFilter = ({searchResults}) => {
     const {hasHotTub,  hasWifi,  hasPatio,  hasFireplace, hasKitchen } = useContext(SearchContext)
+    const [isMobile, setisMobile] = useState(window.matchMedia('screen and (min-device-width : 320px) and (max-width : 768px) and (min-device-height : 480px) and (max-device-height : 1076px)'));
+
+    isMobile.addEventListener('change', (e) => {
+    setisMobile((window.matchMedia('screen and (min-device-width : 320px) and (max-width : 768px) and (min-device-height : 480px) and (max-device-height : 1076px)')))
+  });
+
     const desiredAmenities = getDesiredAmenities(hasHotTub, hasWifi, hasPatio, hasFireplace, hasKitchen)
 
     const filteredLocations = searchResults.filter(result =>  compareDesiredAmenitiesToCurrentGetawaysAmenities (desiredAmenities, result))
     return(
     <>
-    <div className="searchResultsContainer">
-      {filteredLocations.length ? filteredLocations.map( getaway => {
-        return <SingleGetawaySearchOrBrowseResult getaway={getaway} key={getaway.id}/>
+    <div className="searchResultsContainer" id="searchResultsContainer">
+      {filteredLocations.length ? filteredLocations.map( getaway => { 
+        if(getaway && isMobile.matches){return <MobileSingleGetawaySearchOrBrowseResult getaway={getaway} key={getaway.id}/>}else if(getaway && !isMobile.matches){return <SingleGetawaySearchOrBrowseResult getaway={getaway} key={getaway.id}/>}
+        // return <SingleGetawaySearchOrBrowseResult getaway={getaway} key={getaway.id}/>
       }): <h1>No getaways found</h1>}
     </div>
     
