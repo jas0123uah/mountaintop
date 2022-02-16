8a5945dd43693bf89ca91c1b00260e0f6698b1ae
jas0123uah <jas0123@uah.edu>
2022-02-16 17:15:14 -0500

    Detect mobile viewport.

--- react-app/src/components/ProfilePageComponents/PF/PF.js
+++ react-app/src/components/ProfilePageComponents/PF/PF.js
@@ -1,15 +1,48 @@
 
 
-import React from 'react';
+import React, {useState} from 'react';
+import {useLocation} from 'react-router-dom'
 import SideBar from '../SideBar';
 import ProfileMainContent from '../ProfileMainContent'
 import './PF.css'
 export const PF = () => {
+    const location = useLocation();
+    const [isMobile, setisMobile] = useState(window.matchMedia('screen and (min-device-width : 320px) and (max-width : 768px) and (min-device-height : 480px) and (max-device-height : 1076px)'));
+    const [disp, setDisp] = useState("0");
+    
+
+    const adjustHamburgerSideBar =() =>{
+        const newdisp = disp == '0'? '100' : '0'
+        setDisp(newdisp);
+    }
+
+  isMobile.addEventListener('change', (e) => {
+    setisMobile((window.matchMedia('screen and (min-device-width : 320px) and (max-width : 768px) and (min-device-height : 480px) and (max-device-height : 1076px)')))
+  });
+
+  let pfMainContent = null
+  let content;
+
+  if (isMobile && location.pathname.includes("/profile/getaways")) {
+      content = <SideBar/>
+    
+      
+  }
+  else if(isMobile && location.pathname.includes("/profile/getaways")==false){
+      content=<ProfileMainContent/>
+
+  }
+  else{
+      content = <SideBar/>
+      pfMainContent = <div class="div2" id="pf"><ProfileMainContent /> </div>
+  }
 return(
     <>
     <div className="containerForSidebarAndMainContent">
-<div class="div1"><SideBar></SideBar> </div>
-<div class="div2" id="pf"><ProfileMainContent></ProfileMainContent> </div>
+    {content}
+    {pfMainContent}
+{/* <div class="div1"><SideBar></SideBar> </div> */}
+{/* <div class="div2" id="pf"><ProfileMainContent></ProfileMainContent> </div> */}
     </div>
 </>)
 }
