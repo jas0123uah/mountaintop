ab53dcfb7e07a2f2b8a2384e58a45121d469ec38
jas0123uah <jas0123@uah.edu>
2022-02-23 09:47:12 -0500

    Image uploads via AWS

--- react-app/src/components/GetawayComponents/NewGetaway/index.js
+++ react-app/src/components/GetawayComponents/NewGetaway/index.js
@@ -5,6 +5,7 @@ import {createGetaway} from '../../../store/getaways'
 import {loadGetaways} from '../../../store/getaways'
 import {authenticate} from '../../../store/session'
 import {checkURL} from '../../../utils/helperFunctions'
+import $ from 'jquery'
 export const NewGetaway = () => {
   const [errors, setErrors] = useState([]);
   const [address, setAddress] = useState('');
@@ -24,6 +25,25 @@ export const NewGetaway = () => {
   const [img3, setImg3] = useState('')
   const [img4, setImg4] = useState('')
   const [img5, setImg5] = useState('')
+  const [img1Preview, setImg1Preview] = useState('')
+  const [img2Preview, setImg2Preview] = useState('')
+  const [img3Preview, setImg3Preview] = useState('')
+  const [img4Preview, setImg4Preview] = useState('')
+  const [img5Preview, setImg5Preview] = useState('')
+  
+  const [savedImg1File, setSavedImg1File] = useState('')
+  const [savedImg2File, setSavedImg2File] = useState('')
+  const [savedImg3File, setSavedImg3File] = useState('')
+  const [savedImg4File, setSavedImg4File] = useState('')
+  const [savedImg5File, setSavedImg5File] = useState('')
+
+  const [savedImg1Preview, setSavedImg1Preview] = useState('')
+  const [savedImg2Preview, setSavedImg2Preview] = useState('')
+  const [savedImg3Preview, setSavedImg3Preview] = useState('')
+  const [savedImg4Preview, setSavedImg4Preview] = useState('')
+  const [savedImg5Preview, setSavedImg5Preview] = useState('')
+  
+  
   const [validImg1, setValidImg1] = useState(true)
   const [validImg2, setValidImg2] = useState(true)
   const [validImg3, setValidImg3] = useState(true)
@@ -35,72 +55,168 @@ export const NewGetaway = () => {
   const [hasFireplace, setHasFireplace] = useState(false)
   const [hasKitchen, setHasKitchen] = useState(false)
 
-  const img1IsValid = (img) => {
-    if(!img.length){
-      setValidImg1(true)
-      return
-    }
-    if (img.length && checkURL(img) && /^(ftp|http|https):\/\/[^ "]+$/.test(img) == true ) {
-      setValidImg1(true)
-      return 
-    }
-    setValidImg1(false)
-  }
-  const img2IsValid = (img) => {
-    if(!img.length){
-      setValidImg2(true)
-      return
-    }
-    if (img.length && checkURL(img) && /^(ftp|http|https):\/\/[^ "]+$/.test(img) == true ) {
-      setValidImg2(true)
-      return 
-    }
-    setValidImg2(false)
-  }
-  const img3IsValid = (img) => {
-    if(!img.length){
-      setValidImg3(true)
-      return
-    }
-    if (img.length && checkURL(img) && /^(ftp|http|https):\/\/[^ "]+$/.test(img) == true) {
-      setValidImg3(true)
-      return 
-    }
-    setValidImg3(false)
+  const handleImg1Click = () => {
+    $('#img1Upload').trigger('click')
+
   }
 
-  const img4IsValid = (img) => {
-    if(!img.length){
-      setValidImg4(true)
-      return
+  // const img1IsValid = (img) => {
+  //   if(!img.length){
+  //     setValidImg1(true)
+  //     return
+  //   }
+  //   if (img.length && checkURL(img) && /^(ftp|http|https):\/\/[^ "]+$/.test(img) == true ) {
+  //     setValidImg1(true)
+  //     return 
+  //   }
+  //   setValidImg1(false)
+  // }
+  // const img2IsValid = (img) => {
+  //   if(!img.length){
+  //     setValidImg2(true)
+  //     return
+  //   }
+  //   if (img.length && checkURL(img) && /^(ftp|http|https):\/\/[^ "]+$/.test(img) == true ) {
+  //     setValidImg2(true)
+  //     return 
+  //   }
+  //   setValidImg2(false)
+  // }
+  // const img3IsValid = (img) => {
+  //   if(!img.length){
+  //     setValidImg3(true)
+  //     return
+  //   }
+  //   if (img.length && checkURL(img) && /^(ftp|http|https):\/\/[^ "]+$/.test(img) == true) {
+  //     setValidImg3(true)
+  //     return 
+  //   }
+  //   setValidImg3(false)
+  // }
+
+  // const img4IsValid = (img) => {
+  //   if(!img.length){
+  //     setValidImg4(true)
+  //     return
+  //   }
+  //   if (img.length && checkURL(img) && /^(ftp|http|https):\/\/[^ "]+$/.test(img) == true) {
+  //     setValidImg4(true)
+  //     return 
+  //   }
+  //   setValidImg4(false)
+  // }
+
+  // const img5IsValid = (img) => {
+  //   if(!img.length){
+  //     setValidImg5(true)
+  //     return
+  //   }
+  //   if (img.length && checkURL(img) && /^(ftp|http|https):\/\/[^ "]+$/.test(img) == true) {
+  //     setValidImg5(true)
+  //     return 
+  //   }
+  //   setValidImg5(false)
+  // }
+
+  // useEffect(() => {
+  //   img1IsValid(img1)
+  //   img2IsValid(img2)
+  //   img3IsValid(img3)
+  //   img4IsValid(img4)
+  //   img5IsValid(img5)
+
+  // }, [img1, img2, img3, img4, img5])
+  const handleSetImg1 = (e) => {
+        let file = e.target.files[0];
+
+        setImg1(e.target.files[0]);
+        if (file) {
+            setSavedImg1File(file);
+
+            file = URL.createObjectURL(file);
+            setImg1Preview(file);
+            setSavedImg1Preview(file)
+        } else {
+            setImg1(savedImg1File);
+            setImg1Preview(savedImg1Preview);
+        }
     }
-    if (img.length && checkURL(img) && /^(ftp|http|https):\/\/[^ "]+$/.test(img) == true) {
-      setValidImg4(true)
-      return 
+  const handleSetImg2 = (e) => {
+        let file = e.target.files[0];
+
+        setImg2(e.target.files[0]);
+        if (file) {
+            setSavedImg2File(file);
+
+            file = URL.createObjectURL(file);
+            setImg2Preview(file);
+            setSavedImg2Preview(file)
+        } else {
+            setImg2(savedImg2File);
+            setImg2Preview(savedImg2Preview);
+        }
     }
-    setValidImg4(false)
+  const handleSetImg3 = (e) => {
+        let file = e.target.files[0];
+
+        setImg3(e.target.files[0]);
+        if (file) {
+            setSavedImg3File(file);
+
+            file = URL.createObjectURL(file);
+            setImg3Preview(file);
+            setSavedImg3Preview(file)
+        } else {
+            setImg3(savedImg3File);
+            setImg3Preview(savedImg3Preview);
+        }
+    }
+  const handleSetImg4 = (e) => {
+        let file = e.target.files[0];
+
+        setImg4(e.target.files[0]);
+        if (file) {
+            setSavedImg4File(file);
+
+            file = URL.createObjectURL(file);
+            setImg4Preview(file);
+            setSavedImg4Preview(file)
+        } else {
+            setImg4(savedImg4File);
+            setImg4Preview(savedImg4Preview);
+        }
+    }
+  const handleSetImg5 = (e) => {
+        let file = e.target.files[0];
+
+        setImg5(e.target.files[0]);
+        if (file) {
+            setSavedImg5File(file);
+
+            file = URL.createObjectURL(file);
+            setImg5Preview(file);
+            setSavedImg5Preview(file)
+        } else {
+            setImg5(savedImg5File);
+            setImg5Preview(savedImg5Preview);
+        }
+    }
+  const handleImg2Click = () => {
+    $('#img2Upload').trigger('click')
+
   }
+  const handleImg3Click = () => {
+    $('#img3Upload').trigger('click')
 
-  const img5IsValid = (img) => {
-    if(!img.length){
-      setValidImg5(true)
-      return
-    }
-    if (img.length && checkURL(img) && /^(ftp|http|https):\/\/[^ "]+$/.test(img) == true) {
-      setValidImg5(true)
-      return 
-    }
-    setValidImg5(false)
   }
+  const handleImg4Click = () => {
+    $('#img4Upload').trigger('click')
 
-  useEffect(() => {
-    img1IsValid(img1)
-    img2IsValid(img2)
-    img3IsValid(img3)
-    img4IsValid(img4)
-    img5IsValid(img5)
+  }
+  const handleImg5Click = () => {
+    $('#img5Upload').trigger('click')
 
-  }, [img1, img2, img3, img4, img5])
+  }
 
   useEffect(()=> {
     const getaway = {name, address, city, state, name, price, description, numGuests, numBeds, numBaths, numBedrooms, img1, img2, img3, img4, img5}
@@ -139,10 +255,10 @@ export const NewGetaway = () => {
     }
 
 
-    if ((img1.length && !checkURL(img1)) || (img2.length && !checkURL(img2)) || (img3.length && !checkURL(img3)) || (img4.length && !checkURL(img4)) || (img5.length && !checkURL(img5)) || (img1.length && /^(ftp|http|https):\/\/[^ "]+$/.test(img1) == false) || (img2.length && /^(ftp|http|https):\/\/[^ "]+$/.test(img2) == false) ||(img3.length && /^(ftp|http|https):\/\/[^ "]+$/.test(img3) == false) || (img4.length && /^(ftp|http|https):\/\/[^ "]+$/.test(img4) == false) || (img5.length && /^(ftp|http|https):\/\/[^ "]+$/.test(img5) == false)) {
-      errors.push(`Fields in red are invalid image urls.`)
+    // if ((img1.length && !checkURL(img1)) || (img2.length && !checkURL(img2)) || (img3.length && !checkURL(img3)) || (img4.length && !checkURL(img4)) || (img5.length && !checkURL(img5)) || (img1.length && /^(ftp|http|https):\/\/[^ "]+$/.test(img1) == false) || (img2.length && /^(ftp|http|https):\/\/[^ "]+$/.test(img2) == false) ||(img3.length && /^(ftp|http|https):\/\/[^ "]+$/.test(img3) == false) || (img4.length && /^(ftp|http|https):\/\/[^ "]+$/.test(img4) == false) || (img5.length && /^(ftp|http|https):\/\/[^ "]+$/.test(img5) == false)) {
+    //   errors.push(`Fields in red are invalid image urls.`)
       
-    }
+    // }
     setErrors(errors);
   }, [name, address, city, state, name, price, description, numGuests, numBeds, numBaths, numBedrooms, img1, img2, img3, img4, img5])
 
@@ -161,17 +277,56 @@ export const NewGetaway = () => {
   const handleSubmitGetaway = async (e) => {
     e.preventDefault();
     if(!errors.length){
+      const formData = new FormData()
+      formData.append("img1", img1)
+      formData.append("img2", img2)
+      formData.append("address", address)
+      formData.append("city", city)
+      formData.append("state", state)
+      formData.append("latitude", latitude)
+      formData.append("longitude", longitude)
+      formData.append("name", name)
+      formData.append("price", price)
+      formData.append("description", description)
+      formData.append("numGuests", numGuests)
+      formData.append("numBeds", numBeds)
+      formData.append("numBaths", numBaths)
+      formData.append("numBedrooms", numBedrooms)
+      formData.append("userId", userId)
+      formData.append("img3", img3)
+      formData.append("img4", img4)
+      formData.append("img5", img5)
+      formData.append("country", "United States")
+
+      formData.append("hasHotTub", hasHotTub)
+      formData.append("hasWifi", hasWifi)
+      formData.append("hasPatio", hasPatio)
+      formData.append("hasFireplace", hasFireplace)
+      formData.append("hasKitchen", hasKitchen)
       await dispatch(
-        createGetaway({address, city, state, latitude, longitude, name, price, description, numGuests, numBeds, numBaths,  numBedrooms, userId, img1, img2, img3, img4, img5,  hasHotTub, hasWifi, hasPatio, hasKitchen, hasFireplace })
+
+
+        
+
+
+        createGetaway(formData)
         ).catch(async (res) => {
           const data = await res.json();if (data && data.errors) setErrors(data.errors);
         });
         await dispatch(loadGetaways()).then((res) => dispatch (authenticate())).then((res) => history.push("/profile"))
         }
+    // if(!errors.length){
+    //   await dispatch(
+    //     createGetaway({address, city, state, latitude, longitude, name, price, description, numGuests, numBeds, numBaths,  numBedrooms, userId, img1, img2, img3, img4, img5,  hasHotTub, hasWifi, hasPatio, hasKitchen, hasFireplace })
+    //     ).catch(async (res) => {
+    //       const data = await res.json();if (data && data.errors) setErrors(data.errors);
+    //     });
+    //     await dispatch(loadGetaways()).then((res) => dispatch (authenticate())).then((res) => history.push("/profile"))
+    //     }
     }
   return (
     <div className='formWrapper'>
-    <form onSubmit={ (e) => handleSubmitGetaway(e)}>
+    <form onSubmit={ (e) => handleSubmitGetaway(e)} className='getawayForm'>
       <fieldset className='formflex'>
         <legend>New Getaway</legend>
         <div className="form-fields-flex">
@@ -303,7 +458,7 @@ export const NewGetaway = () => {
         />
       </div>
 
-    <fieldset className="amenities-fieldset">              
+    <fieldset className="amenities-fieldset" id="amenities-fieldset">              
     <legend>Amenities</legend>             
     <input id='hottub' type="checkbox" checked={hasHotTub} onChange={(e) => {setHasHotTub(!hasHotTub)}}/>Hot tub
     <input id='wifi' type="checkbox" checked={hasWifi} onChange={(e) => {setHasWifi(!hasWifi)}}/>WiFi  
@@ -313,14 +468,66 @@ export const NewGetaway = () => {
                     
   </fieldset> 
 
+
+
       <div className="getawayImgFields">
-        <label htmlFor='imgUrl' className={`is-red-${validImg1}`}>Image URL</label>
-        <input id='img1' type="url" className={`IMGS`} name="imgUrl"   onBlur={(e)=>{setImg1(e.target.value)}} />
+        
+        <i class="fa fa-camera" onClick={handleImg1Click}></i>
+        <input type="file" id="img1Upload" className="fas fa-file-upload" name="imgUrl" accept=".jpg, .png, .jpeg"  onChange={(e)=>{handleSetImg1(e)}} />
+        {img1 ? <img src={img1Preview} className="getawayImageUpload" alt="" /> : <span>Upload an image</span>}
+
+      </div>
+
+      <div className="getawayImgFields">
+        
+        <i class="fa fa-camera" onClick={handleImg2Click}></i>
+        <input type="file" id="img2Upload" className="fas fa-file-upload" name="imgUrl" accept=".jpg, .png, .jpeg"  onChange={(e)=>{handleSetImg2(e)}} />
+        {img2 ? <img src={img2Preview} className="getawayImageUpload" alt="" /> : <span>Upload an image</span>}
+
+      </div>
+
+      <div className="getawayImgFields">
+        
+        <i class="fa fa-camera" onClick={handleImg3Click}></i>
+        <input type="file" id="img3Upload" className="fas fa-file-upload" name="imgUrl" accept=".jpg, .png, .jpeg"  onChange={(e)=>{handleSetImg3(e)}} />
+        {img3 ? <img className="getawayImageUpload" src={img3Preview} alt="" /> : <span>Upload an image</span>}
+
+      </div>
+
+      <div className="getawayImgFields">
+        
+        <i class="fa fa-camera" onClick={handleImg4Click}></i>
+        <input type="file" id="img4Upload" className="fas fa-file-upload" name="imgUrl" accept=".jpg, .png, .jpeg"  onChange={(e)=>{handleSetImg4(e)}} />
+        {img4 ? <img src={img4Preview} alt="" className="getawayImageUpload" /> : <span>Upload an image</span>}
+
+      </div>
+
+      <div className="getawayImgFields">
+        
+        <i class="fa fa-camera" onClick={handleImg5Click}></i>
+        <input type="file" id="img5Upload" className="fas fa-file-upload" name="imgUrl" accept=".jpg, .png, .jpeg"  onChange={(e)=>{handleSetImg5(e)}} />
+        {img5 ? <img src={img5Preview} className="getawayImageUpload" alt="" /> : <span>Upload an image</span>}
 
       </div>
 
 
 
+
+
+
+
+      
+
+
+
+       {/* 
+       <div className="getawayImgFields">
+        <label htmlFor='imgUrl' className={`is-red-${validImg1}`}>Image URL</label>
+        <input id='img1' type="url" className={`IMGS`} name="imgUrl"   onBlur={(e)=>{setImg1(e.target.value)}} />
+
+      </div>
+       
+       
        <div className="getawayImgFields">
         <label htmlFor='imgUrl' className={`is-red-${validImg2}`}>Image URL</label>
         <input id='img2' className="IMGS" type="url" name="imgUrl"  onBlur={(e)=>{setImg2(e.target.value)}} />
@@ -345,7 +552,7 @@ export const NewGetaway = () => {
         <label htmlFor='imgUrl' className={`is-red-${validImg5}`}>Image URL</label>
         <input id='img5'type="url" className="IMGS" name="imgUrl"   onBlur={(e)=>{setImg5(e.target.value)}} />
 
-      </div>
+      </div> */}
 
       
       <div className="submitImgFlexbox">
